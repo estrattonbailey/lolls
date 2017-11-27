cbd7ef9dee12fd30085bc95479054e9e9e23e739
estrattonbailey <ericstrattonbailey@gmail.com>
2017-11-27 17:09:53 -0500

    Rewrite with UMD

--- package/index.js
+++ package/index.js
@@ -1,22 +1,30 @@
-const ls = localStorage
-const _set = ls.setItem
-const _get = ls.getItem
-const _del = ls.removeItem
-
-const isExp = exp => exp < Date.now()
-const getExp = s => !s ? 0 : Date.now() + (s * 1000)
-
-export default ls ? {
-  get: key => {
-    const { data, exp } = JSON.parse(_get(key))
-    const e = isExp(exp)
-    e && _del(key)
-    return e ? null : data
-  },
-  set: (key, data, s = null)  => {
-    _set(key, JSON.stringify({
-      data,
-      exp: getExp(s)
-    }))
+(function (root, factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory(root)
+  } else {
+    root.lolls = factory(root)
   }
-} : {}
+}(this, function (root) {
+  const ls = root.localStorage
+  const _set = ls.setItem
+  const _get = ls.getItem
+  const _del = ls.removeItem
+
+  const isExp = exp => exp < Date.now()
+  const getExp = s => !s ? 0 : Date.now() + (s * 1000)
+
+  return {
+    get: key => {
+      const { data, exp } = JSON.parse(_get(key))
+      const e = isExp(exp)
+      e && _del(key)
+      return e ? null : data
+    },
+    set: (key, data, s = null)  => {
+      _set(key, JSON.stringify({
+        data,
+        exp: getExp(s)
+      }))
+    }
+  }
+}))
